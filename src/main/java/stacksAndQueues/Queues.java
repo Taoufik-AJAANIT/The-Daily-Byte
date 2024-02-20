861f21368fc20498312d472c0e2fe75192d0e0cf
taoufik <taoufik.ajaanit.1@gmail.com>
2024-02-20 17:10:03 +0100

    Stacks & Queues/Moving Average

--- src/main/java/stacksAndQueues/Queues.java
+++ src/main/java/stacksAndQueues/Queues.java
@@ -2,9 +2,14 @@ package main.java.stacksAndQueues;
 
 public class Queues {
     Queue<Integer> queue;
+    private static int size = 3;
+    private int sum;
+    private int count;
+    private int[] window;
 
     public Queues() {
         this.queue = new Queue<>();
+        this.window = new int[size];
 
     }
 
@@ -14,5 +19,26 @@ public class Queues {
         }
         queue.add(tmp);
         return queue.size;
+    }
+
+    public int next(int val) throws Exception {
+        if (this.queue.size == size) {
+            int element = this.queue.remove();
+            this.sum = this.sum - element;
+        }
+        queue.add(val);
+        this.sum = this.sum + val;
+        return this.sum / queue.size;
+    }
+
+
+    //    using circular array
+    public int next1(int val) {
+        int index = count % size;
+        count++;
+        sum = sum - window[index];
+        sum = sum + val;
+        window[index] = val;
+        return sum / Math.min(size, count);
     }
 }
--- src/test/main/java/QueuesTest.java
+++ src/test/main/java/QueuesTest.java
@@ -17,10 +17,28 @@ class QueuesTest {
 
     @Test
     @DisplayName("Call Counter")
-    void checkIfIsPalindromeWithRemovalRecursive() throws Exception {
+    void callCounter() throws Exception {
         assertEquals(1, queues.ping(1));
         assertEquals(2, queues.ping(300));
         assertEquals(3, queues.ping(3000));
         assertEquals(1, queues.ping(7000));
+    }
+
+    @Test
+    @DisplayName("Moving Average")
+    void averageFromDataStream() throws Exception {
+        assertEquals(3, queues.next(3));
+        assertEquals(4, queues.next(5));
+        assertEquals(5, queues.next(7));
+        assertEquals(6, queues.next(6));
+    }
+
+    @Test
+    @DisplayName("Moving Average with array and modulo")
+    void averageFromDataStreamWithArray() throws Exception {
+        assertEquals(3, queues.next1(3));
+        assertEquals(4, queues.next1(5));
+        assertEquals(5, queues.next1(7));
+        assertEquals(6, queues.next1(6));
     }
 }
