10b062ab00708377aa9b95fb5a321e4b1adfd156
taoufik <taoufik.ajaanit.1@gmail.com>
2024-02-25 00:14:13 +0100

    Binary Trees/Lowest Common Ancestor

--- src/main/java/trees/binaryTree/BinaryTrees.java
+++ src/main/java/trees/binaryTree/BinaryTrees.java
@@ -1,5 +1,10 @@
 package main.java.trees.binaryTree;
 
+
+import main.java.linkedlists.LinkedList;
+
+import java.sql.PreparedStatement;
+
 public class BinaryTrees<T extends Comparable<T>> {
     public Node find(T value, Node<T> root) {
         if (root == null) return null;
@@ -9,5 +14,30 @@ public class BinaryTrees<T extends Comparable<T>> {
             return find(value, root.left);
         }
         return find(value, root.right);
+    }
+
+    public LinkedList toSortedLinkedList(Node<T> root) throws Exception {
+        if (root == null) return null;
+        LinkedList result = new LinkedList();
+        return toSortedLinkedListRecc(root, result);
+
+    }
+
+    private LinkedList toSortedLinkedListRecc(Node<T> root, LinkedList result) throws Exception {
+        if (root == null) return result;
+        toSortedLinkedListRecc(root.left, result);
+        result.addLast(root.value);
+        toSortedLinkedListRecc(root.right, result);
+        return result;
+    }
+
+    public Node lowestCommonAncestor(Node<Integer> root, int num1, int num2) {
+        if (root.value <= Math.max(num1, num2) && root.value >= Math.min(num1, num2)) {
+            return root;
+        } else if (root.value > Math.max(num1, num2)) {
+            return lowestCommonAncestor(root.left, num1, num2);
+        } else {
+            return lowestCommonAncestor(root.right, num1, num2);
+        }
     }
 }
--- src/test/main/java/BinaryTreesTest.java
+++ src/test/main/java/BinaryTreesTest.java
@@ -1,5 +1,6 @@
 package test.main.java;
 
+import main.java.linkedlists.LinkedList;
 import main.java.trees.binaryTree.BinaryTrees;
 import main.java.trees.binaryTree.Node;
 import org.junit.jupiter.api.BeforeEach;
@@ -10,14 +11,15 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class BinaryTreesTest {
     private BinaryTrees binaryTrees;
+
     @BeforeEach
-    void setUp(){
+    void setUp() {
         this.binaryTrees = new BinaryTrees();
     }
 
     @Test
     @DisplayName("Find Value")
-    void find(){
+    void find() {
         Node root = new Node(3);
         root.left = new Node(1);
         root.right = new Node(4);
@@ -32,11 +34,74 @@ class BinaryTreesTest {
         assertEquals(root2.right, this.binaryTrees.find(9, root2));
 
 
-
         Node root3 = new Node(8);
         root3.left = new Node(6);
         root3.right = new Node(9);
         assertEquals(null, this.binaryTrees.find(7, root3));
+    }
+
+    @Test
+    @DisplayName("Convert Binary Search Tree to Sorted Linked List")
+    void toSortedLinkedList() throws Exception {
+        Node root = new Node(5);
+        root.left = new Node(1);
+        root.right = new Node(6);
+        LinkedList<Integer> a = new LinkedList();
+        a.addLast(1);
+        a.addLast(5);
+        a.addLast(6);
+        assertEquals(a, this.binaryTrees.toSortedLinkedList(root));
+
+
+        Node root2 = new Node(5);
+        root2.left = new Node(2);
+        root2.right = new Node(9);
+        root2.left.left = new Node(1);
+        root2.left.right = new Node(3);
+
+        LinkedList<Integer> b = new LinkedList();
+        b.addLast(1);
+        b.addLast(2);
+        b.addLast(3);
+        b.addLast(5);
+        b.addLast(9);
+        assertEquals(b, this.binaryTrees.toSortedLinkedList(root2));
+
+
+        Node root3 = new Node(5);
+        root3.right = new Node(6);
+
+        LinkedList<Integer> c = new LinkedList();
+        c.addLast(5);
+        c.addLast(6);
+        assertEquals(c, this.binaryTrees.toSortedLinkedList(root3));
+    }
+
+    @Test
+    @DisplayName("Lowest Common Ancestor")
+    void lowestCommonAncestor() throws Exception {
+        Node root = new Node(7);
+        root.left = new Node(2);
+        root.left.left = new Node(1);
+        root.left.right = new Node(5);
+        root.right = new Node(9);
+
+        assertEquals(root, this.binaryTrees.lowestCommonAncestor(root, 1, 9));
+
+        Node root2 = new Node(8);
+        root2.left = new Node(3);
+        root2.left.left = new Node(2);
+        root2.left.right = new Node(6);
+        root2.right = new Node(9);
+
+        assertEquals(root2.left, this.binaryTrees.lowestCommonAncestor(root2, 2, 6));
+
+        Node root3 = new Node(8);
+        root3.left = new Node(6);
+        root3.right = new Node(9);
+
+        assertEquals(root3, this.binaryTrees.lowestCommonAncestor(root3, 6, 8));
+
     }
 
 }
